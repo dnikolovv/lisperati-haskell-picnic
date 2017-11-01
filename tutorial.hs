4df08c468f4a614c2ae59ba48540f0d6549a5cdc
Dobromir Nikolov <dobromir.nikolov@mentormate.com>
2017-11-01 15:27:14 +0200

    Simple .svg file drawing.

--- tutorial.hs
+++ tutorial.hs
@@ -15,6 +15,21 @@ type TemperatureFunction           = Int -> Int -> Float
 type TransitionProbabilityFunction = Int -> Int -> Float -> Float
 type MotionFunction a              = StdGen -> a -> (StdGen,a)
 
+writePoint :: Point -> String 
+writePoint (x,y) = (show x)++","++(show y)++" "
+
+writePolygon :: (Color,Polygon) -> String 
+writePolygon ((r,g,b),p) = "<polygon points=\""++(concatMap writePoint p)++"\" style=\"fill:#cccccc;stroke:rgb("++(show r)++","++(show g)++","++(show b)++");stroke-width:2\"/>"
+
+writePolygons :: [(Color,Polygon)] -> String 
+writePolygons p = "<svg xmlns=\"http://www.w3.org/2000/svg\">"++(concatMap writePolygon p)++"</svg>"
+
+colorize :: Color -> [Polygon] -> [(Color,Polygon)] 
+colorize = zip.repeat
+
+rainbow :: [[Polygon] -> [(Color, Polygon)]]
+rainbow@[red,green,blue,yellow,purple,teal] = map colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
+
 main :: IO ()
 main = do 
   putStr "Hello World! Let's have a picnic! \n"
@@ -25,3 +40,5 @@ main = do
 
   putStr "Number of people coming: "
   print (length people)
+  
+  writeFile "tut0.svg" $ writePolygons (red [[(100,200),(200,100),(200,200),(100,200)],[(200,200),(300,200),(300,200),(200,300)]])
