cc5e1226f0b70026c7c0016afeb42997356f0485
Dobromir Nikolov <dobromir.nikolov@mentormate.com>
2017-11-01 15:59:39 +0200

    Finding concrete picnic blanket placement points.

--- tutorial.hs
+++ tutorial.hs
@@ -61,6 +61,51 @@ sliceY :: Float -> [Polygon] -> ([Polygon],[Polygon])
 sliceY y = slice ((y >).snd) interpolateY
     where interpolateY (x1,y1) (x2,y2) = (x1+(x2-x1)*(y-y1)/(y2-y1),y) 
 
+boundingRect :: [Polygon] -> (Float,Float,Float,Float) 
+boundingRect p = (minimum xs,minimum ys,maximum xs,maximum ys)
+    where xs = map fst $ concat p
+          ys = map snd $ concat p
+
+halveTriangles :: Int -> [Polygon] -> ([Polygon],[Polygon]) 
+halveTriangles n p = let (l,t,r,b) = boundingRect p         
+                         f = fromIntegral n         
+                         h = fromIntegral $ div n 2 
+                        in if r-l > b-t
+                        then sliceX ((r*h+l*(f-h))/f) p
+                        else sliceY ((b*h+t*(f-h))/f) p
+
+distance :: Point -> Point -> Float 
+distance p1 p2 = sqrt (deltax*deltax+deltay*deltay)
+    where deltax = (fst p1)-(fst p2)
+          deltay = (snd p1)-(snd p2)
+
+area :: Polygon -> Float 
+area [a,b,c] = let x = distance a b
+                   y = distance b c
+                   z = distance c a
+                   s = (x+y+z)/2
+                in sqrt (s*(s-x)*(s-y)*(s-z))
+
+allocatePeople :: Int -> [Polygon] -> [[Polygon]] 
+allocatePeople 0 _ = []
+allocatePeople 1 t = [t]
+allocatePeople n t = let (t1,t2) = halveTriangles n t            
+                         a1      = sum $ map area t1             
+                         a2      = sum $ map area t2             
+                         f       = round $ (fromIntegral n)*a1/(a1+a2) 
+                        in (allocatePeople f t1)++(allocatePeople (n-f) t2)
+
+findLotCenter :: [Polygon] -> Point 
+findLotCenter p = let (l,t,r,b) = boundingRect p      
+                      m@(x,y)   = ((r+l)/2,(b+t)/2)   
+                      (lh,rh)   = sliceX x p          
+                      (th,bh)   = sliceY y $ lh ++ rh 
+                      centerOrder p1 p2 = compare (distance p1 m) (distance p2 m) 
+                    in minimumBy (comparing $ distance m) $ concat $ th ++ bh
+
+makeDot :: Point -> Polygon 
+makeDot (x,y) = [(x-2,y-2),(x+2,y-2),(x+2,y+2),(x-2,y+2)]                        
+    
 main :: IO ()
 main = do 
   putStr "Hello World! Let's have a picnic! \n"
@@ -86,3 +131,12 @@ main = do
   let (left_side,right_side) = sliceX 200 triangles
   
   writeFile "tut3.svg" $ writePolygons $ (red left_side) ++ (blue right_side)
+
+  let lots = allocatePeople (length people) triangles
+
+  writeFile "tut4.svg" $ writePolygons $ concat $ zipWith ($) (cycle rainbow) lots
+
+  let centers = map findLotCenter lots
+  let spots = blue $ map makeDot centers
+
+  writeFile "tut5.svg" $ writePolygons $ (green park) ++ spots
