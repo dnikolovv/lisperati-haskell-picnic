d9f3cf1245e9992b68281e5ca438358358dded71
Dobromir Nikolov <dobromir.nikolov@mentormate.com>
2017-11-01 15:43:42 +0200

    .svg file reading and splitting the map in triangles.

--- tutorial.hs
+++ tutorial.hs
@@ -30,6 +30,19 @@ colorize = zip.repeat
 rainbow :: [[Polygon] -> [(Color, Polygon)]]
 rainbow@[red,green,blue,yellow,purple,teal] = map colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
 
+readPoint :: String -> Point
+readPoint s | Just [x,y] <- matchRegex (mkRegex "([0-9.]+),([0-9.]+)") s = (read x,read y)
+
+readPolygon :: String -> Polygon
+readPolygon = (map readPoint).(splitRegex $ mkRegex " L ")
+
+readPolygons :: String -> [Polygon]
+readPolygons = (map readPolygon).tail.(splitRegex $ mkRegex "<path")
+
+triangulate :: Polygon -> [Polygon]
+triangulate (a:b:c:xs) = [a,b,c]:triangulate (a:c:xs)
+triangulate _ = []                                                
+
 main :: IO ()
 main = do 
   putStr "Hello World! Let's have a picnic! \n"
@@ -42,3 +55,12 @@ main = do
   print (length people)
   
   writeFile "tut0.svg" $ writePolygons (red [[(100,200),(200,100),(200,200),(100,200)],[(200,200),(300,200),(300,200),(200,300)]])
+
+  park_data <- readFile "park.svg" 
+  let park = readPolygons park_data
+
+  writeFile "tut1.svg" $ writePolygons (green park)
+
+  let triangles = concatMap triangulate park
+  
+  writeFile "tut2.svg" $ writePolygons (purple triangles)
